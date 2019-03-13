e11e829677032fa9afe5591685756c28bf794b90
Robert Papagna <rpapagna@e1z2r5p8.42.us.org>
2019-03-12 17:32:06 -0700

    new main process with error codes

--- main.c
+++ main.c
@@ -3,28 +3,35 @@
 int     main(int argc, char **argv)
 {
 	int fd;
-	int ret;
-	size_t count;
-	char buff [22];
+	int err_num;
+	
 	char *line;
 
 	if (argc != 2)
-		return (-1);
+		return (1);
 	else
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd > 2)
-		{
-			count = 0;
-			while((ret = get_next_line(fd, &line)) > 0)
-			{
-				if (count++ == 6)
-					count = 1;
-				if ((ret = check_next_line(line, count)) < 0)
-					return (-1);
-			}
-		}
+		err_num = check_next_tetra(fd, line);
 		close(fd);
+		ERR_MAC(err_num);
 	}
 	return (0);
+}
+
+unsigned char	ft_error(unsigned int err_num)
+{
+	char	*err_msgs[4];
+
+	if (!err_num)
+		return (0);
+	err_msgs[0] = "";
+	err_msgs[1] = ": invalid file descriptor";
+	err_msgs[2] = ": invalid input format";
+	err_msgs[3] = ": get_next_line failed";
+	ft_putstr("error");
+	if (ERR_SW)
+		ft_putstr(err_msgs[err_num - 1]);
+	ft_putchar('\n');
+	return (1);
 }
