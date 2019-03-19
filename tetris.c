4799c5174b708910a4ec259a772de331470e645a
Robert Papagna <rpapagna@e1z3r6p23.42.us.org>
2019-03-19 12:21:45 -0700

    .

--- tetris.c
+++ tetris.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 23:43:40 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/03/19 12:19:56 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/03/19 12:21:27 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,15 @@ void    *convertit(t_tetra *tetramino, char **tetra)
 		if (found && tetra[y][x]){
 			if (tetra[y][x] == '#' && found == '1'){
 				tetramino->block1 = {y - anc_y, x - anc_x}; //convert x,y according to anchors x & y
-				found == '2';
+				found = '2';
 			}
 			else if (tetra[y][x] == '#' && found == '2'){
 				tetramino->block2 = {y - anc_y, x - anc_x}; //store x,y to int arr element of struct
-				found == '3';
+				found = '3';
 			}
 			else if (tetra[y][x] == '#' && found == '3'){
 				tetramino->block3 = {y - anc_y, x - anc_x}; //create indication of last block found
-				found == '4';
+				found = '4';
 			 }
 		 }
 		if (found == '4')
