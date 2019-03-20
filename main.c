86b954bdafd45374112cfd94794eb3fb71a73cc0
Robert Papagna <rpapagna@e1z2r8p11.42.us.org>
2019-03-19 21:52:40 -0700

    .

--- main.c
+++ main.c
@@ -6,18 +6,20 @@
 /*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 22:53:26 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/03/18 22:53:27 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/03/19 18:56:23 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
+
 int     main(int argc, char **argv)
 {
-	int						fd;
-	int						err_num;
-	char					*tetra[5];
-	t_tetra				*tet_arr;
+	int		fd;
+	int		err_num;
+	size_t	sqsz;
+	char	letter_id;
+	char	*tetra[5];
+	t_tetra	*tet_arr;
 	if (argc != 2)
 		return (1);
 	else
@@ -27,20 +29,23 @@ int     main(int argc, char **argv)
 		tetra[2] = NULL;
 		tetra[3] = NULL;
 		tetra[4] = NULL;
+		letter_id = '@';
 		fd = open(argv[1], O_RDONLY);
-
 		while (!(err_num = checkit(fd, tetra)))
 		{
+			++letter_id;
 			// new funcs
 			if (!tet_arr){
-				if(!(tet_arr = ft_newtetra(tetra))){
+				if(!(tet_arr = ft_newtetra(tetra, letter_id))){
 					return (ft_error(5));	}}
 			//else
 			//	tet_add(t_tetra **tet_arr, t_tetra *new)
 		}
 			//free(*tetra);
 		close(fd);
-printf("%c",tet_arr->letter_id); // test print
+//		ft_putchar(tetra[3][1]);
+//		ft_putchar(tet_arr->letter_id);
+//	printf("%c",tet_arr->letter_id); // test print
 		if (err_num != -1)
 			ERR_MAC(err_num);
 	}
