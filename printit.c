714e379997d13d9c71d399cc36fc934c31c9c162
Robert Papagna <rpapagna@e1z2r8p10.42.us.org>
2019-03-21 22:02:00 -0700

    proper map printing

--- printit.c
+++ printit.c
@@ -6,27 +6,31 @@
 /*   By: jmbomeyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 16:20:69 by jmbomeyo          #+#    #+#             */
-/*   Updated: 2019/03/19 21:58:44 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/03/20 19:00:44 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char			**mapinit(size_t sqsz)
+char		**mapinit(size_t sqsz, char ***amap)
 {
 	char	**map;
 	size_t	i;
 
-	if (!(map = (char **)(malloc(sizeof(char *) * sqsz))))
-		return (NULL);
+	IF_EXIT(!(map = (char **)(malloc(sizeof(char *) * sqsz))), NULL);
 	i = -1;
 	while (++i < sqsz)
-		if (!(map[i] = ft_strcnew(sqsz, '.')))
-			return (NULL);
+	{
+		if (*amap)
+			map[i] = ft_strdup((*amap)[i]);
+		else
+			map[i] = ft_strcnew(sqsz, '.');
+		IF_EXIT(!map[i], NULL);
+	}
 	return (map);
 }
 
-static int		dropit(char ***amap, int sqsz, int b[4][2], char chr)
+static int	dropit(char ***amap, int sqsz, int b[4][2], char chr)
 {
 	char	**map;
 
@@ -51,39 +55,36 @@ static int		dropit(char ***amap, int sqsz, int b[4][2], char chr)
 	return (0);
 }
 
-static t_tetra	*takeit(t_tetra *tetra_list, char id)
+t_tetra		*takeit(t_tetra *tetra_list, char id)
 {
 	while (tetra_list && tetra_list->letter_id != id)
 		tetra_list = tetra_list->next;
 	return (tetra_list);
 }
 
-int				fitit(char ***amap, t_tetra *tet_arr, size_t sqsz, char *order)
+int		fitit(char ***amap, t_tetra *tetra, size_t sqsz)
 {
 	size_t	i;
-	t_tetra	*tetra_falling;
-	int		b[4][2];
+	int	b[4][2];
 
 	i = -1;
-	tetra_falling = takeit(tet_arr, *order);
 	while (++i < sqsz * sqsz)
 	{
 		b[0][0] = (int)(i / sqsz);
 		b[0][1] = (int)(i % sqsz);
-		b[1][0] = b[0][0] + (tetra_falling->block1)[0];
-		b[1][1] = b[0][1] + (tetra_falling->block1)[1];
-		b[2][0] = b[0][0] + (tetra_falling->block2)[0];
-		b[2][1] = b[0][1] + (tetra_falling->block2)[1];
-		b[3][0] = b[0][0] + (tetra_falling->block3)[0];
-		b[3][1] = b[0][1] + (tetra_falling->block3)[1];
-		if (dropit(amap, (int)sqsz, b, tetra_falling->letter_id))
-			if (!(tetra_falling = takeit(tet_arr, *(++order))))
-				return (0);
+		b[1][0] = b[0][0] + (tetra->block1)[0];
+		b[1][1] = b[0][1] + (tetra->block1)[1];
+		b[2][0] = b[0][0] + (tetra->block2)[0];
+		b[2][1] = b[0][1] + (tetra->block2)[1];
+		b[3][0] = b[0][0] + (tetra->block3)[0];
+		b[3][1] = b[0][1] + (tetra->block3)[1];
+		if (dropit(amap, (int)sqsz, b, tetra->letter_id))
+			return (0);
 	}
 	return (-1);
 }
 
-int				printit(char **map, size_t sqsz)
+int		printit(char **map, size_t sqsz)
 {
 	size_t	i;
 
