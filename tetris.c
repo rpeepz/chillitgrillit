13c7d82fa9b02db53758f2316571ba827c7b35f5
Robert Papagna <rpapagna@e1z2r8p15.42.us.org>
2019-03-26 19:40:48 -0700

    added function to make norm

--- tetris.c
+++ tetris.c
@@ -12,6 +12,59 @@
 
 #include "fillit.h"
 
+void		incrementit(int i[2])
+{
+	if (i[1] == 3)
+	{
+		i[1] = 0;
+		(i[0])++;
+	}
+	else
+		(i[1])++;
+}
+
+void		anchorit(int *anchor, int *i)
+{
+	anchor[0] = i[0];
+	anchor[1] = i[1];
+	if (i[1] == 3)
+	{
+		i[1] = 0;
+		(i[0])++;
+	}
+	else
+		(i[1])++;
+}
+
+void		coordinateit(char **tetra, int anc[2], int i[2], t_tetra **t)
+{
+	char	found;
+
+	found = 0;
+	while (found < 3)
+	{
+		if (tetra[i[0]][i[1]] == '#' && found == 0)
+		{
+			((**t).block1)[0] = i[0] - anc[0];
+			((**t).block1)[1] = i[1] - anc[1];
+			found = 1;
+		}
+		else if (tetra[i[0]][i[1]] == '#' && found == 1)
+		{
+			((**t).block2)[0] = i[0] - anc[0];
+			((**t).block2)[1] = i[1] - anc[1];
+			found = 2;
+		}
+		else if (tetra[i[0]][i[1]] == '#' && found == 2)
+		{
+			((**t).block3)[0] = i[0] - anc[0];
+			((**t).block3)[1] = i[1] - anc[1];
+			break ;
+		}
+		incrementit(i);
+	}
+}
+
 void		*convertit(t_tetra *tetramino, char **tetra)
 {
 	int		i[2];
@@ -27,38 +80,12 @@ void		*convertit(t_tetra *tetramino, char **tetra)
 			(i[1])++;
 		if (tetra[i[0]][i[1]] == '#' && found == 0)
 		{
-			anc[0] = i[0];
-			anc[1] = i[1];
-			found = 1;
-			(i[1])++;
-		}
-		while (found && tetra[i[0]][i[1]])
-		{
-			if (tetra[i[0]][i[1]] == '#' && found == 1)
-			{
-				(tetramino->block1)[0] = i[0] - anc[0];
-				(tetramino->block1)[1] = i[1] - anc[1];
-				found = 2;
-			}
-			else if (tetra[i[0]][i[1]] == '#' && found == 2)
-			{
-				(tetramino->block2)[0] = i[0] - anc[0];
-				(tetramino->block2)[1] = i[1] - anc[1];
-				found = 3;
-			}
-			else if (tetra[i[0]][i[1]] == '#' && found == 3)
-			{
-				(tetramino->block3)[0] = i[0] - anc[0];
-				(tetramino->block3)[1] = i[1] - anc[1];
-				found = 4;
-			}
-			(i[1])++;
-		}
-		if (found == 4)
+			anchorit(anc, i);
+			coordinateit(tetra, anc, i, &tetramino);
 			break ;
+		}
 		(i[0])++;
 	}
-	tetramino->letter_id = tetra[4][0];
 	return (tetramino);
 }
 
@@ -67,28 +94,9 @@ int			ft_newtetra(char **tetra, char letter_id, t_tetra **atet)
 	t_tetra	*tetramino;
 
 	IF_EXIT(!(tetramino = (t_tetra *)malloc(sizeof(*tetramino))), 0);
-	IF_EXIT(!(tetramino->block1 = (int *)malloc(sizeof(int) * 2)), 0);
-	IF_EXIT(!(tetramino->block2 = (int *)malloc(sizeof(int) * 2)), 0);
-	IF_EXIT(!(tetramino->block3 = (int *)malloc(sizeof(int) * 2)), 0);
 	convertit(tetramino, tetra);
 	tetramino->letter_id = letter_id;
 	tetramino->next = NULL;
 	tet_append(atet, tetramino);
 	return (1);
-}
-
-int			tet_append(t_tetra **head, t_tetra *new)
-{
-	t_tetra	*etk;
-
-	etk = *head;
-	if (!etk)
-		*head = new;
-	else
-	{
-		while (etk->next)
-			etk = etk->next;
-		etk->next = new;
-	}
-	return (0);
 }
