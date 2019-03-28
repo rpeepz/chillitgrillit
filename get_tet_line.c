55ab622d5ecc121ee396ebc9bd7b2c7a9a25925b
Robert Papagna <rpapagna@e1z2r8p8.42.us.org>
2019-03-27 20:59:32 -0700

    configuration and renaming

--- get_tet_line.c
+++ get_tet_line.c
@@ -1,45 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_tet_line.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 18:10:19 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/03/14 16:42:52 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/03/27 16:42:52 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		get_next_line(const int fd, char **line)
-{
-	static char		*str[1];
-	char			*tmp;
-	char			buf[BUFF_SIZE + 1];
-	int				i;
-
-	if (fd < 0 || line == NULL || BUFF_SIZE == 0)
-		return (-1);
-	while (((i = read(fd, buf, BUFF_SIZE)) > 0))
-	{
-		buf[i] = '\0';
-		if (!str[0])
-			str[0] = ft_strnew(0);
-		tmp = ft_strjoin(str[0], buf);
-		free(str[0]);
-		str[0] = tmp;
-		if (ft_strchr(buf, '\n'))
-			break ;
-	}
-	if (i < 0)
-		return (-1);
-	else if (i == 0 && (!str[0] || !str[0][0]))
-		return (0);
-	return (ft_afterline(str, line));
-}
-
-int		ft_afterline(char **s, char **line)
+static int	ft_afterline(char **s, char **line)
 {
 	char	*tmp;
 	int		len;
@@ -66,4 +39,38 @@ int		ft_afterline(char **s, char **line)
 		ft_strdel(&s[0]);
 	}
 	return (1);
+}
+
+int			get_tet_line(const int fd, char **line)
+{
+	static char		*str[1];
+	char			*tmp;
+	char			buf[BUFF_SIZE + 1];
+	int				i;
+
+	if (fd < 0 || line == NULL || BUFF_SIZE == 0)
+		return (-1);
+	while (((i = read(fd, buf, BUFF_SIZE)) > 0))
+	{
+		buf[i] = '\0';
+		if (!str[0])
+			str[0] = ft_strnew(0);
+		tmp = ft_strjoin(str[0], buf);
+		free(str[0]);
+		str[0] = tmp;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (i < 0)
+		return (-1);
+	else if (i == 0 && (!str[0] || !str[0][0]))
+		return (0);
+	return (ft_afterline(str, line));
+}
+
+t_tetra		*find_tetra(t_tetra *tetra_list, char id)
+{
+	while (tetra_list && tetra_list->letter_id != id)
+		tetra_list = tetra_list->next;
+	return (tetra_list);
 }
