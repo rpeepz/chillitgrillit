309ef3b472aed3cd22f091f0ce511fdffe28e2df
Robert Papagna <rpapagna@e1z2r8p13.42.us.org>
2019-04-05 13:25:26 -0700

    process ordering

--- .debug.c
+++ .debug.c
@@ -6,15 +6,11 @@
 /*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 22:53:26 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/03/22 18:07:16 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/04/05 13:17:56 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#undef ERR_SW
-#define ERR_SW 1
-#undef DEBUG
-#define DEBUG 1
 
 static void		initit(char **tetra, int *err_num, \
 						char *letter_id, t_tetra **tet_arr)
@@ -38,33 +34,6 @@ static int		loopit(int chr)
 	return (chr + 1);
 }
 
-unsigned char	ft_error(unsigned int err_num)
-{
-	char	*err_msgs[9];
-
-	if (err_num == 0)
-	{
-		ft_putendl("usage: ./fillit ... <filename>");
-		return (1);
-	}
-	if (!err_num)
-		return (0);
-	err_msgs[0] = ": get_next_line failed";
-	err_msgs[1] = ": invalid file descriptor";
-	err_msgs[2] = ": five line or more tetramino found";
-	err_msgs[3] = ": forbidden char found";
-	err_msgs[4] = ": get_next_line failed";
-	err_msgs[5] = ": bad tetramino found";
-	err_msgs[6] = ": they're after me lucky charms!";
-	err_msgs[7] = ": ";
-	err_msgs[8] = ": Too many tetras";
-	ft_putstr("error");
-	if (ERR_SW)
-		ft_putstr(err_msgs[err_num - 1]);
-	ft_putchar('\n');
-	return (1);
-}
-
 int				main(int argc, char **argv)
 {
 	int		fd;
@@ -78,37 +47,16 @@ int				main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	while (!err_num)
 	{
-		err_num = checkit(fd, tetra);
-		letter_id = loopit(letter_id);
-		if (err_num <= 0)
+		if ((err_num = checkit(fd, tetra)) > 0)
+			break ;
+		if ((letter_id = loopit(letter_id)) && err_num <= 0)
 			if (!(combineit(tetra, letter_id, &tetrominos)))
 				return (ft_error(5));
 	}
 	close(fd);
-	if (err_num != -1)
-		if (ft_error(err_num))
-			return (1);
+	IF_EXIT(((err_num != -1) && ft_error(err_num)), 1);
 	solveit(tetrominos);
+	tetra_freeit(tetrominos);
+	line_freeit(tetra, 4);
 	IF_EXIT(system("leaks fillit"), 0);
-}
-
-int				printit(char **map, int sqsz)
-{
-	int		i;
-
-	i = -1;
-	while (++i < sqsz)
-	{
-		if (DEBUG)
-		{
-			ft_putchar('[');
-			ft_putnbr(i);
-			ft_putstr("] ");
-		}
-		ft_putstr(map[i]);
-		if (DEBUG)
-			ft_putchar('$');
-		ft_putchar('\n');
-	}
-	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 22:53:26 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/03/28 19:07:16 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/04/05 13:14:22 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,10 @@ static void		initit(char **tetra, int *err_num, \
 	*tet_arr = NULL;
 }
 
-static int		loopit(int chr, int *err_num, int fd)
+static int		loopit(int chr, int *err_num)
 {
 	if (chr == 'Z')
-	{
-		close(fd);
 		*err_num = 8;
-	}
 	return (chr + 1);
 }
 
@@ -48,17 +45,16 @@ int				main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	while (!err_num)
 	{
-		err_num = checkit(fd, tetra);
-		if ((letter_id = loopit(letter_id, &err_num, fd)) && err_num <= 0)
+		if ((err_num = checkit(fd, tetra)) > 0)
+			break ;
+		if ((letter_id = loopit(letter_id, &err_num)) && err_num <= 0)
 			if (!(combineit(tetra, letter_id, &tetrominos)))
 				return (ft_error(5));
 	}
 	close(fd);
-	fd = 0;
-	while (fd < 5)
-		free(tetra[fd++]);
 	IF_EXIT(((err_num != -1) && ft_error(err_num)), 1);
 	solveit(tetrominos);
 	tetra_freeit(tetrominos);
+	line_freeit(tetra, 4);
 	return (0);
 }
